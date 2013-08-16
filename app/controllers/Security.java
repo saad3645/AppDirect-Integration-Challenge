c0e9a4259a632e254b38e77ca1ef66a9d3d8ae79
Saad Ahmed <Saad@SAAD_LAPTOP.(none)>
2013-08-16 11:41:14 -0500

    Update

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -7,12 +7,17 @@ import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 import org.apache.http.client.methods.HttpGet;
+import play.Play;
 
 public class Security {
 
 
     public static String signAuthorization(String url) throws OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException {
-        OAuthConsumer consumer = new CommonsHttpOAuthConsumer("Dummy", "secret");
+
+        String consumerKey = Play.application().configuration().getString("appdirect.consumerKey");
+        String consumerSecret = Play.application().configuration().getString("appdirect.consumerSecret");
+
+        OAuthConsumer consumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
         HttpGet request = new HttpGet(url);
         consumer.sign(request);
         return request.getHeaders("Authorization")[0].getValue();
