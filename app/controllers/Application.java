2d3dc4f144e876171d145a3931c00fbd7f9ac78a
Saad Ahmed <Saad@SAAD_LAPTOP.(none)>
2013-08-20 12:17:10 -0500

    OpenId Integration

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,13 +3,18 @@ package controllers;
 import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
+import org.openid4java.association.AssociationException;
 import org.openid4java.consumer.ConsumerException;
 import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.consumer.VerificationResult;
 import org.openid4java.discovery.DiscoveryException;
 import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.discovery.Identifier;
 import org.openid4java.message.AuthRequest;
 import org.openid4java.message.MessageException;
+import org.openid4java.message.ParameterList;
 import play.*;
+import play.cache.Cache;
 import play.mvc.*;
 
 import views.html.*;
@@ -26,7 +31,7 @@ public class Application extends Controller {
     }
 
 
-    public static Result authenticate() {
+    public static Result openIdLogIn() {
 
         Map<String, String[]> queryParams = request().queryString();
 
@@ -53,7 +58,7 @@ public class Application extends Controller {
 
         try {
             ConsumerManager manager = new ConsumerManager();
-            String returnUrl = "http://mighty-spire.herokuapp.com/openId";
+            String returnUrl = "http://mighty-spire.herokuapp.com/openid/validate";
 
             // perform discovery on the user-supplied identifier
             List discoveries = manager.discover(openId);
@@ -62,30 +67,57 @@ public class Application extends Controller {
             // and retrieve one service endpoint for authentication
             DiscoveryInformation discovered = manager.associate(discoveries);
 
+            // store the discovery information in the cache for later use
+            Cache.set(openId, discovered, 60 * 15);
+
             // obtain a AuthRequest message to be sent to the OpenID provider
             AuthRequest authRequest = manager.authenticate(discovered, returnUrl);
 
             return redirect(authRequest.getDestinationUrl(true));
         }
 
-        catch (DiscoveryException e) {
+        catch (Exception e) {
             e.printStackTrace();
-            return ok(e.getMessage());
-        } catch (MessageException e) {
-            e.printStackTrace();
-            return ok(e.getMessage());
-        } catch (ConsumerException e) {
-            e.printStackTrace();
-            return ok(e.getMessage());
+            return ok("Oops! something went wrong");
         }
-
 
         //return ok("OpenId: " + openId + " accountId: " + accountId);
     }
 
 
-    public static Result openId() {
-        return ok();
+    public static Result openIdValidate() {
+
+        Map<String, String[]> queryParams = request().queryString();
+
+        String claimedId = queryParams.get("openid.claimed_id")[0];
+
+        // retrieve the previously stored discovery information
+        DiscoveryInformation discovered = (DiscoveryInformation)Cache.get(claimedId);
+
+        String receivingUri = request().uri();
+        ParameterList parameterList = new ParameterList(queryParams);
+
+        ConsumerManager manager = new ConsumerManager();
+
+        // verify the response
+        try {
+            VerificationResult verification = manager.verify(receivingUri, parameterList, discovered);
+            Identifier verified = verification.getVerifiedId();
+
+            if (verified != null) {
+                return ok("Login Successful");
+            }
+
+            else {
+                return unauthorized(receivingUri);
+            }
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+            return unauthorized("Unauthorized");
+        }
+
     }
   
 }
